e1f06c37ca62b4fa9c66679c52925d049e6072bd
bonisiweinnocent <bonisiwe.cukatha@gmail.com>
2022-04-25 15:22:37 +0200

    asc order

--- api.js
+++ api.js
@@ -94,7 +94,7 @@ module.exports = function (app, db) {
 	});
 
 	app.get('/api/garments/grouped', async function (req, res) {
-		const result = await db.many(`select count(*), gender from garment group by gender`);
+		const result = await db.many(`select count(*), gender from garment group by gender desc`);
 		
 		res.json({
 			data: result
